e85138a57fe5b93a8b75a73f3ccc8b681afe3b50
Mohamed asem <mohamedasemsyam@outlook.com>
2019-06-03 04:53:31 +0200

    Add project files.

---
+++ Yackeen Geeks Task/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using Yackeen_Geeks_Task.Models;
+
+[assembly: OwinStartupAttribute(typeof(Yackeen_Geeks_Task.Startup))]
+namespace Yackeen_Geeks_Task
+{
+    public partial class Startup
+    {
+        private ApplicationDbContext _dbContext;
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
